77067dddf19ccb661008318ab551e3e011b0ca9c
Sixto José Romero Martínez <sixto.jose@gmail.com>
2020-12-09 12:07:24 -0500

    Se crea form login

--- lib/presentation/login/widgets/genericbutton.dart
+++ lib/presentation/login/widgets/genericbutton.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class GenericButton extends StatelessWidget {  
-  GenericButton({this.onTap, this.child});
+  GenericButton({this.onTap, this.child, this.backgroundColor});
   final Widget child;
   final VoidCallback onTap;
+  final Color backgroundColor;
 
   @override
   Widget build(BuildContext context) {
     return ButtonTheme(
+      
       minWidth: MediaQuery.of(context).size.width * 0.44,
       child: FlatButton(
         shape: RoundedRectangleBorder(
@@ -16,11 +18,15 @@ class GenericButton extends StatelessWidget {
             color: Color(0XFFCDD2DB),
           )
         ),
-        color: Colors.white,
+        color: backgroundColor,
         onPressed: onTap,
-        child: Container(        
-          height: 24,
-          child: child
+        child: Center(
+          child: Container(        
+            height: 24,
+            child: Center(
+              child: child
+            ),
+          ),
         ),
       ),
     );
